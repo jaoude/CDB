8b794a77f6b06b304038251b4337e6f91ab7f04e
charbelnoun <charbel-noun@hotmail.com>
2018-11-14 16:54:12 +0200

    update 11-14

---
+++ CDB.DAL/Migrations/20181114134756_added_bytearray_forfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CDB.DAL.Migrations
+{
+    public partial class added_bytearray_forfile : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "FileByte",
+                table: "Document",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FileByte",
+                table: "Document");
+        }
+    }
+}
